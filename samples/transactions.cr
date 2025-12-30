d7d5fabaa8f67e56fb2e764090cd7554f7903dbd
Anton Maminov <anton.linux@gmail.com>
2025-12-30 14:00:19 +0200

    extract format_table

--- samples/transactions.cr
+++ samples/transactions.cr
@@ -1,5 +1,28 @@
 require "../src/monobank_api"
 
+def format_table(
+  data : Array(Hash(String, _)),
+  numeric_keys : Set(String) = Set(String).new,
+  separator = " | ",
+) : String
+  return "" if data.empty?
+
+  keys = data.first.keys
+
+  width_by_key = keys.to_h do |key|
+    {key, data.max_of(&.[key].to_s.size)}
+  end
+
+  data.join('\n') do |row|
+    keys.join(separator) do |key|
+      value = row[key]?.to_s || ""
+      width = width_by_key[key]
+
+      numeric_keys.includes?(key) ? value.rjust(width) : value.ljust(width)
+    end
+  end
+end
+
 token = ARGV[0]? || raise "Usage: statements.cr <token>"
 client = MonobankApi::Client.new(token)
 
@@ -16,29 +39,12 @@ data = statements.map do |statement|
   balance = (statement.balance / 100.0).format(decimal_places: 2)
 
   {
-    :time        => statement.time,
-    :description => statement.description,
-    :amount      => amount,
-    :balance     => balance,
-    :mcc         => statement.mcc,
+    "time"        => statement.time,
+    "description" => statement.description,
+    "amount"      => amount,
+    "balance"     => balance,
+    "mcc"         => statement.mcc,
   }
 end
 
-keys = data.first.keys
-
-width_by_key = keys.to_h do |k|
-  {k, data.max_of(&.[k].to_s.size)}
-end
-
-numeric_keys = Set{:amount, :balance, :mcc}
-
-data.each do |row|
-  line = keys.join(" | ") do |key|
-    value = row[key].to_s
-    width = width_by_key[key]
-
-    numeric_keys.includes?(key) ? value.rjust(width) : value.ljust(width)
-  end
-
-  puts line
-end
+puts format_table(data)
