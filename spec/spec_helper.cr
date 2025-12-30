8619e503d7b99ef2052e9b07a311da7dd20728fb
Anton Maminov <anton.linux@gmail.com>
2025-12-30 17:05:50 +0200

    add specs

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,8 @@
 require "spec"
+require "vcr"
 require "../src/monobank_api"
+
+VCR.configure do |settings|
+  settings.cassette_library_dir = File.join(__DIR__, "cassettes")
+  settings.filter_sensitive_data["X-Token"] = "<TOKEN>"
+end
