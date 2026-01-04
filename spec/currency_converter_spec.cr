d3b59e562eecc2908814b273340933bb87fe47cd
Anton Maminov <anton.linux@gmail.com>
2026-01-04 13:29:16 +0200

    Update currency_converter_spec.cr

--- spec/currency_converter_spec.cr
+++ spec/currency_converter_spec.cr
@@ -1,12 +1,10 @@
 require "./spec_helper"
 
-# Specs for currency conversion utility
 describe MonobankApi::CurrencyConverter do
-  it "converts using live rates from cassette" do
+  it "converts using live rates" do
     VCR.use_cassette("currencies") do
       converter = MonobankApi::CurrencyConverter.new
 
-      # From cassette: USD/UAH rateBuy 42.1, EUR/UAH rateSell 50.2008
       converter.convert(100, "USD", "UAH").should be_close(4205.0, 0.0001)
       converter.convert(100, "USD", "EUR").should be_close(84.8896, 0.01)
       converter.convert(100, "EUR", "USD").should be_close(116.8, 0.01)
